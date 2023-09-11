f28028be1f0a3804e52d4cefe0d69c7e3b2e9f9d
mbubula6 <142213441+mbubula6@users.noreply.github.com>
2023-09-11 18:00:46 +0200

    static public to public static

--- src/Dwa.java
+++ src/Dwa.java
@@ -1,5 +1,5 @@
 public class Dwa {
-    static public void main(String [] args) {
+    public static void main(String [] args) {
         //11.09.2023
         System.out.println("Jan Kowalski\nZielona 234/2\nNowy Sącz 36-800\n111111111");
 
