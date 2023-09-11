3730265e1b0a8c74eb3b7d7ba2ab6f2c1459e536
mbubula6 <142213441+mbubula6@users.noreply.github.com>
2023-09-11 18:03:54 +0200

    static public to public static

--- src/Siedem.java
+++ src/Siedem.java
@@ -1,5 +1,5 @@
 public class Siedem {
-    static public void main( String [] args ) {
+    public static void main( String [] args ) {
         double sprzedazRegionMazowieckiProcent = 0.62;
         double calkowitaSprzedaz = 4600000;
         double sprzedazRegionMazowiecki = calkowitaSprzedaz * sprzedazRegionMazowieckiProcent;
